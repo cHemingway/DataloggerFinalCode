609726492bd5d6b9aaeedde0c1418bce2c24fcd3
Chris Hemingway <chris.hemingway@gmail.com>
2014-04-24 14:46:36 +0100

    Add initial start and stop commands.

--- Sources/CommandParser/netprot_command_list.h
+++ Sources/CommandParser/netprot_command_list.h
@@ -4,12 +4,15 @@
 #include "netprot_command.h"
 #include "netprot_commands_misc.h"
 #include "netprot_commands_setget.h"
+#include "netprot_commands_startstop.h"
 #include <stddef.h>
 
 netprot_command netprot_default_command_list[] = {
 	{"PING", netprot_cmd_ping},
 	{"SET", netprot_cmd_set},
 	{"GET", netprot_cmd_get},
+	{"START", netprot_cmd_start},
+	{"STOP", netprot_cmd_stop},
 	{"RESET", netprot_cmd_reset},
 	{"",NULL} /* Terminate with null values */
 };
---
+++ Sources/CommandParser/netprot_commands_startstop.h
@@ -0,0 +1,7 @@
+#ifndef NETPROT_COMMAND_START_H_
+#define NETPROT_COMMAND_START_H_
+
+int netprot_cmd_start(const char *in, char *out, int outlen);
+int netprot_cmd_stop(const char *in, char *out, int outlen);
+
+#endif /* NETPROT_COMMAND_START_H_ */
