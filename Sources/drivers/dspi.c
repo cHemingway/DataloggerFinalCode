8621ec92a027a65289ba328fb68dec9b70c4aec1
Chris Hemingway <chris.hemingway@gmail.com>
2014-05-15 16:17:49 +0100

    Remove last dregs of dspi_common.h

--- Sources/drivers/dspi.c
+++ Sources/drivers/dspi.c
@@ -1,25 +1,11 @@
 #include "derivative.h"
-#include "dspi_common.h"
-
-/* definitions */
-
-#define CORE_SRAM_ADDR 0x20000000 
-
-/* prototypes */
 
 //initialise pin muxing for all three DSPI modules
 void dspi_init(void);
 
 //send and receive data, no fifo enabled, input args:  DSPI0, DSPI1, or DSPI2
-uint16_t dspi_no_fifo_3(uint32_t master);
 
-/* globals */
-volatile struct	SPI_MemMap *ptrDSPI[NUM_DSPI] = {SPI0_BASE_PTR, SPI1_BASE_PTR, SPI2_BASE_PTR};
-
-
-/********************************************************************/
-/* Initialise all ports for DSPI */
-/********************************************************************/
+/* Initialise ports for DSPI */
 void dspi_init(void) 
 {
   /* clock gate */
@@ -30,7 +16,7 @@ void dspi_init(void)
   GPIOB_PDDR |= ~0x08;           //enable PTB3 as input 
   
   PORTE_PCR0 &= ~PORT_PCR_MUX_MASK;
-  PORTE_PCR0 |= PORT_PCR_MUX(2); //SPI1_PCS0   //PTE0 --- leave this enabled even though not physically used to avoid problems
+  PORTE_PCR0 |= PORT_PCR_MUX(2); //SPI1_PCS0   //PTE0 --- leave enabled even though not physically used to avoid problems
   
   PORTE_PCR2 &= ~PORT_PCR_MUX_MASK;
   PORTE_PCR2 |= PORT_PCR_MUX(2); //SPI1_SCK    //PTE2
@@ -43,32 +29,27 @@ void dspi_init(void)
 uint16_t dspi_no_fifo_3(uint32_t master)
 {
 	//master mode, flush rx fifo, flush tx fifo, halt transfer
-	ptrDSPI[master]->MCR = 0x80000C01;
-	
-	/********************************************************************************/
+	SPI1_MCR = 0x80000C01;
 	
 	//master mode, shift incoming data into shift register, ADC inactive state is high, halt transfer
-	ptrDSPI[master]->MCR = 0x81010001;
+	SPI1_MCR = 0x81010001;
 	
 	//frame size is 16bits, divide fsys by 1024 => fnew ~ 10kHz??
-	ptrDSPI[master]->CTAR[0] = 0x78000001;
- 
-	/* Send Data */
+	SPI1_CTAR0 = 0xf8000001;
 	
-	//Assert PCS bit 16, put (0x0000) into master pushr, needed for shift regs. errors otherwise
-	ptrDSPI[master]->PUSHR = 0x00010000;
+	//Assert PCS bit 16, put data into master pushr (0x0000), needed for shift regs. errors otherwise.
+	SPI1_PUSHR = 0x00010000;
 
-	// Clear HALT bit
-	ptrDSPI[master]->MCR &= 0xFFFFFFFE;
+	// Clear halt bit - start transfer
+	SPI1_MCR &= 0xFFFFFFFE;
 	
-	//wait while
-	while((ptrDSPI[master]->SR & SPI_SR_TCF) == 0){;}  
-	ptrDSPI[master]->SR = ptrDSPI[master]->SR | SPI_SR_TCF;
+	//wait while all bits are clocked in
+	while((SPI1_SR & SPI_SR_TCF_MASK) == 0){;}  
+	SPI1_SR = SPI1_SR | SPI_SR_TCF_MASK;
 		
-	/* halt */
-	// set HALT bit to stop transfer
-	ptrDSPI[master]->MCR |= 1;
+	// set halt bit - stop transfer
+	SPI1_MCR |= 1;
 	
-	/* read data */
-	return ptrDSPI[master]->POPR;
-}
+	//read data from POPR
+	return SPI1_POPR;
+};
